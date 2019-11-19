5bb2608cbdb1a4f0bc86deb2ee209c71194892dc
HungPv <hungcon.5070@gmail.com>
2019-11-19 15:21:28 +0700

    [W2][AddHomeComponent]

--- Front/chat_react/src/Component/Template/Home.js
+++ Front/chat_react/src/Component/Template/Home.js
@@ -1,32 +1,147 @@
-import React from 'react';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import React , { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    link: {
-        textDecoration: 'none',
-        color: 'red',
-    },
-    button : {
-        marginRight: theme.spacing(2),
-    },
-  }),
-);
 
-function Home() {
-    const classes = useStyles();
-    return (
-        <h1 className={classes.link}>Home</h1>
-    );
+const useStyles = makeStyles(theme => ({
+  menuTitle: {
+    flexGrow: 1,
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+    
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
+const cards = [1, 2, 3];
+
+export default function Home() {
+  const classes = useStyles();
+  const [menuState, setMenuState] = useState({anchorEl: null});
+
+  const openMenu = (event) => {
+    const anchorEl = event.currentTarget;
+    setMenuState({anchorEl: anchorEl});
+  };
+
+  const closeMenu = () => {
+    setMenuState({anchorEl: null});
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap className={classes.menuTitle}>
+            Hung Con
+          </Typography>
+          <React.Fragment>
+            < IconButton edge="start" className={classes.icon} color="inherit" aria-label="menu" onClick={openMenu}>
+              <MenuIcon />
+            </IconButton>
+            <Menu anchorEl={menuState.anchorEl} open={Boolean(menuState.anchorEl)} onClose={closeMenu}>
+                <MenuItem  >Settings</MenuItem>
+                <MenuItem  >Sign out</MenuItem>
+            </Menu>  
+          </React.Fragment>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Album layout
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Something short and leading about the collection below—its contents, the creator, etc.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Main call to action
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Send Message
+                    </Button>
+                    <Button size="small" color="primary">
+                      Unfriend
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </React.Fragment>
+  );
 }
-
-export default Home;
